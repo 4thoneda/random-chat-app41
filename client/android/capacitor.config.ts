8eef94de50f1061446c57c690d47217bbeccaccc
8thoneda <gpamemories008@gmail.com>
2025-07-22 01:09:53 +0530

    Added capacitor.config.ts

---
+++ client/android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ajnabicam.app',
+  appName: 'AjnabiCam',
+  webDir: 'dist'
+};
+
+export default config;
